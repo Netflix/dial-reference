dcb66b3641b7f91615af6f911b033177c91d72ad
Chintan Parikh <cparikh@netflix.com>
2017-10-02 10:47:15 -0700

    Fix build errors on Ubuntu Xenial 64 bit. Add -fPIC flag for building the lib, also add -Wl,--allow-shlib-undefined in the client LDFLAGS.

--- client/build.sh
+++ client/build.sh
@@ -1,5 +1,4 @@
-TARGET=/usr/local/i686-netflix-linux-gnu-4.3/bin/i686-netflix-linux-gnu- \
-LDFLAGS="-L/usr/local/i686-netflix-linux-gnu-4.3/netflix/lib \
--Wl,-rpath,/usr/local/i686-netflix-linux-gnu-4.3/netflix/lib" \
-INCLUDES=-I/usr/local/i686-netflix-linux-gnu-4.3/netflix/include \
+export TARGET=/usr/local/i686-netflix-linux-gnu-4.3/bin/i686-netflix-linux-gnu-
+export LDFLAGS="-L/usr/local/i686-netflix-linux-gnu-4.3/netflix/lib -Wl,-rpath,/usr/local/i686-netflix-linux-gnu-4.3/netflix/lib -Wl,--allow-shlib-undefined"
+export INCLUDES=-I/usr/local/i686-netflix-linux-gnu-4.3/netflix/include
 make
